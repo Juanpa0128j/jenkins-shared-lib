4d923cc320c6db3d9015525a622ab9ce9256d282
Juan Pablo Mejía Gómez <66537159+Juanpa0128j@users.noreply.github.com>
2025-08-17 17:41:57 -0500

    Update deploy.groovy

--- vars/deploy.groovy
+++ vars/deploy.groovy
@@ -1,13 +1,17 @@
 def call(String containerName, String image, String port) {
     sh """
-    CID=\$(docker ps -q --filter "name=${containerName}")
+    # Get all containers (running or stopped) with this name
+    CID=\$(docker ps -aq --filter "name=${containerName}")
     if [ ! -z "\$CID" ]; then
         echo "Stopping old container ${containerName}..."
         docker stop \$CID || true
         docker rm \$CID || true
     fi
 
+    # Pull the latest image
     docker pull ${image}
+
+    # Run new container
     docker run -d --name ${containerName} -p ${port}:3000 ${image}
     """
 }
