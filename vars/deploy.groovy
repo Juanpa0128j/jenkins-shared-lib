97599a4a3240f1fd113c2ece502a90fdad2d1d0d
Juan Pablo Mejía Gómez <66537159+Juanpa0128j@users.noreply.github.com>
2025-08-17 17:39:00 -0500

    Update deploy.groovy

--- vars/deploy.groovy
+++ vars/deploy.groovy
@@ -1,6 +1,5 @@
 def call(String containerName, String image, String port) {
     sh """
-    # Stop the old container if it exists
     CID=\$(docker ps -q --filter "name=${containerName}")
     if [ ! -z "\$CID" ]; then
         echo "Stopping old container ${containerName}..."
@@ -8,10 +7,7 @@ def call(String containerName, String image, String port) {
         docker rm \$CID || true
     fi
 
-    # Pull the latest image
     docker pull ${image}
-
-    # Run the new container
     docker run -d --name ${containerName} -p ${port}:3000 ${image}
     """
 }
